66c0fbc8e4c62081aa422d9b282c2b57855f1a59
Isabella Roder <isabellaroder631@gmail.com>
2026-08-22 15:28:05 -0300

    feat: adiciona player e movimento WASD

--- core/src/main/java/com/isabella/lunareth/Lunareth.java
+++ core/src/main/java/com/isabella/lunareth/Lunareth.java
@@ -1,32 +1,40 @@
 package com.isabella.lunareth;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.isabella.lunareth.player.Player;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Lunareth extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    
+    private ShapeRenderer shapeRenderer;
+    private Player player;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        shapeRenderer = new ShapeRenderer();
+        player = new Player(100, 100);
     }
 
     @Override
     public void render() {
+        float delta = Gdx.graphics.getDeltaTime();
+        player.update(delta);
+
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+
+        shapeRenderer.begin(ShapeType.Filled);
+        player.render(shapeRenderer);
+        shapeRenderer.end();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        shapeRenderer.dispose();
     }
 }
---
+++ core/src/main/java/com/isabella/lunareth/player/Player.java
@@ -0,0 +1,32 @@
+package com.isabella.lunareth.player;
+
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class Player {
+    
+    private float x;
+    private float y;
+    private final float tamanho = 32f;
+    private final float velocidade = 200f;
+
+    public Player(float startX, float startY) {
+        this.x = startX;
+        this.y = startY;
+    }
+
+    public void update(float delta) {
+        if (Gdx.input.isKeyPressed(Keys.W)) y += velocidade * delta;
+        if (Gdx.input.isKeyPressed(Keys.S)) y -= velocidade * delta;
+        if (Gdx.input.isKeyPressed(Keys.A)) x -= velocidade * delta;
+        if (Gdx.input.isKeyPressed(Keys.D)) x += velocidade * delta;
+    }
+
+    public void render(ShapeRenderer render) {
+        render.setColor(Color.PURPLE);
+        render.rect(x, y, tamanho, tamanho);
+    }
+}
